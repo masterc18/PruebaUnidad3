b6096dab382c8e65d37d3453f4bf484748a9b2c5
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-06-05 08:11:11 -0600

    Se añadio el archivo menu con su funciom correspondiente

---
+++ menu.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Ejercicio1.cpp"
+#include "Ejercicio2.cpp"
+
+using namespace std;
+
+void menu()
+{
+    int op do
+    {
+
+        cout << "=======================Bienvenido a mi tercera prueba===================" << endl;
+        cout << "1. Ver si un numero es impar o impar " << endl;
+        cout << "2, ver el calculo factorial de un numero " << endl;
+        cout << "3. Salir " << endl;
+        cin >> op
+
+            switch (op)
+        {
+        case 1:
+
+            Numeros();
+            break;
+
+        case 2:
+            CalcularFactorial();
+            break;
+        case 3:
+            cout << "Saliendo............" << endl;
+            break;
+
+        default:
+            cout << "Opcion invalida " << endl;
+            break;
+        }
+    }
+    while (op != 3)
+}
